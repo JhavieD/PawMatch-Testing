450b36aacbab09a32fe0a896760fc587e241255d
Jan Vincent <135355204+JhavieD@users.noreply.github.com>
2025-06-07 19:39:21 +0800

    Revert "Multi Authentication"

--- database/migrations/2024_06_07_000000_create_users_table.php
+++ database/migrations/2024_06_07_000000_create_users_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->string('usertype')->default('user');
-            // ^Multiauth
             $table->string('password');
             $table->string('phone_number');
             $table->string('role');
