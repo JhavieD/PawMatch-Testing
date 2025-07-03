f935aedcfe47719feea11a1e75e2adca1478dc64
Jan Vincent <135355204+JhavieD@users.noreply.github.com>
2025-07-04 01:48:40 +0800

    added notification WIP

---
+++ app/Models/Notification.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    protected $fillable = [
+        'user_id', 'type', 'title', 'message', 'is_read', 'read_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+}
